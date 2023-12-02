e72d04c30726f2dec206b66dd2a2ad77582552bf
adityaroy6770 <147490457+adityaroy6770@users.noreply.github.com>
2023-12-02 11:56:04 +0530

    Add files via upload

---
+++ eq_explore_data.py
@@ -0,0 +1,39 @@
+import json
+from plotly.graph_objs import Scattergeo,Layout
+from plotly import offline
+#explore the structure of data
+filename='data/eq_data_30_day_m1.json'
+with open(filename)as f:
+	all_eq_data=json.load(f)
+readable_file='data/readable_eq_data.json'
+with open(readable_file,'w')as f:
+	json.dump(all_eq_data,f,indent=4)
+all_eq_dicts=all_eq_data['features']
+print(len(all_eq_dicts))
+mags,lons,lats=[],[],[]
+for eq_dict in all_eq_dicts:
+	mag=eq_dict['properties']['mag']
+	lon=eq_dict['geometry'],['coordinates'][0]
+	lat=eq_dict['geometry'],['coordinates'][0]
+	mags.append(mag)
+	lons.append(lon)
+	lats.append(lat)
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])
+#map the earthquake
+data=[{
+    'type':'scattergeo',
+    'lon':lons,
+    'lat':lats,
+    'marker':{
+        'size':[5*mag for mag in mags],
+        'color':mags,
+        'colorscale':'Viridis'
+		'reversescale':True,
+		'colorbar':{'title':'magnitude'},
+},
+}]
+my_layout=Layout(title='global earthquakes')
+fig={'data':data,'layout':my_layout}
+offline.plot(fig,filename='global_earthquakes.html')
